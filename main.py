60d28aa3988b69dcd3648ea84be009a7d293654d
Bjorn Cooley <bjorn@taskrabbit.com>
2019-01-23 18:15:59 -0800

    calculate cost

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import datetime
 from utils.calendar_service import get_google_cal_service
 from utils.sheets_service import get_sheets_service
 
-def main():
-    cal_service = get_google_cal_service()
-    events = get_events(cal_service)
+EMPLOYEES = {}
+DEFAULT_HOURLY = 120000 / 260 / 8
 
+def main():
     sheets_service = get_sheets_service()
     employee_data = get_employee_data(sheets_service)
 
+    cal_service = get_google_cal_service()
+    events = get_events(cal_service)
     display_events(events)
 
 
@@ -36,19 +38,40 @@ def get_employee_data(service):
     if not values:
         print('No data found.')
     else:
-        print('Name, Rate:')
         for row in values:
-            print('%s, %s' % (row[0], row[1]))
+            EMPLOYEES[row[0]] = hourly_from_annual(row[1])
+
+
+def hourly_from_annual(salary):
+    return int(salary) / 260 / 8
 
 
 def display_events(events):
+    total_cost = 0
+
     if not events:
         print('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         if 'attendees' not in event:
             continue
-        print(start, event['attendees'])
+        cost = calculate_cost(event['attendees'])
+        total_cost += cost
+
+        print("%s: %d" % (event['summary'], cost))
+        
+    print("Total cost: %d" % total_cost)
+
+
+def calculate_cost(attendees):
+    cost = 0
+    for a in attendees:
+        key = a['email']
+        if key in EMPLOYEES:
+            cost += EMPLOYEES[key]
+        else:
+            cost += DEFAULT_HOURLY
+    return cost
 
 
 if __name__ == '__main__':
